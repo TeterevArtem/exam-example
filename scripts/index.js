0d1d96be49c16ae11c2d96c1800e261a869df251
Artem <artem@Ulukbeks-Mac-mini.local>
2021-11-19 13:52:53 +0600

    complete without pixel perfect

---
+++ scripts/index.js
@@ -0,0 +1,22 @@
+// const canvas = document.getElementById('canvas');
+// const ctx = canvas.getContext('2d');
+
+// // setting for Retina Display
+// if (window.devicePixelRatio > 1) {
+//     var canvasWidth = canvas.width;
+//     var canvasHeight = canvas.height;
+
+//     canvas.width = canvasWidth * window.devicePixelRatio;
+//     canvas.height = canvasHeight * window.devicePixelRatio;
+//     canvas.style.width = canvasWidth + "px";
+//     canvas.style.height = canvasHeight + "px";
+
+//     ctx.scale(window.devicePixelRatio, window.devicePixelRatio);
+// }
+// let total = 0,
+//     count = 1;
+// while (count <= 10) {
+//     total += count;
+//     count++;
+// }
+// console.log(total);
